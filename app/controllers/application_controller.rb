660cf4629e276225e653268b7fc47a0f10b00da7
Jeremy Hubert <jhubert@gmail.com>
2012-07-15 11:29:37 -0700

    Removed password protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with :name => "change", :password => "theworld"
+#  http_basic_authenticate_with :name => "change", :password => "theworld"
 end
